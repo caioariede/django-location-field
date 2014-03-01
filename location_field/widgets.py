b32f81e3f187787a9cb6444126030e6bc7d8efda
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2014-03-01 02:08:17 -0400

    Fix correct template path

--- location_field/widgets.py
+++ location_field/widgets.py
@@ -49,7 +49,7 @@ class LocationWidget(widgets.TextInput):
 
         text_input = super(LocationWidget, self).render(name, value, attrs)
 
-        return render_to_string('location_field/map_widget', {
+        return render_to_string('location_field/map_widget.html', {
             'field_name': name,
             'field_input': mark_safe(text_input)
         })
