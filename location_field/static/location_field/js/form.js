eab94dadf5ccb7f83c2515c600b5b0cd97d0a932
Caio Ariede <caio.ariede@gmail.com>
2016-01-15 17:08:17 -0200

    Started moving from Google Maps to Leaflet for a broader support for map libraries

--- location_field/static/location_field/js/form.js
+++ location_field/static/location_field/js/form.js
@@ -1,161 +1,81 @@
-($ || django.jQuery)(function($){
-    function location_field_load(map, location_based, zoom, suffix)
-    {
-        var parent = map.parent().parent();
+!function($){
+    $.locationField = function(options) {
+        var LocationField = {
+            options: $.extend({
+                mapType: 'google',
+                mapId: 'map',
+                mapLatLng: '0,0',
+                mapOptions: {
+                    zoom: 9
+                },
+                basedFieldsSelector: [],
+                suffix: '',
+                fixMarker: true
+            }, options),
 
-        var location_map;
+            mapTypes: /google/,
 
-        var location_coordinate = parent.find('input[type=text]');
+            render: function() {
+                if ( ! this.mapTypes.test(this.options.mapType)) {
+                    console.error('invalid map type');
+                }
 
-        function savePosition(point)
-        {
-            if (point) {
-                location_coordinate.val(point.lat().toFixed(6) + "," + point.lng().toFixed(6));
-                location_map.panTo(point);
-            }
-            else {
-                var point = new google.maps.LatLng(1, 1);
-                location_map.panTo(point)
+                var mapOptions = this._getMapOptions(),
+                    map = this._getMap(mapOptions);
+
+                if (mapOptions.center) {
+                    L.marker(mapOptions.center).addTo(map);
+                }
+
+                // fix issue w/ marker not appearing
+                if (this.options.fixMarker)
+                    this.__fixMarker();
+            },
+
+            _getMap: function(mapOptions) {
+                var map = new L.Map(this.options.mapId, mapOptions);
+
+                if (this.options.mapType == 'google') {
+                    map.addLayer(new L.Google('ROADMAP'));
+                }
+
+                return map;
+            },
+
+            _getMapOptions: function() {
+                return $.extend(this.options.mapOptions, {
+                    center: this._getLatLng()
+                });
+            },
+
+            _getLatLng: function() {
+                var l = this.options.mapLatLng.split(',').map(parseFloat);
+                return new L.LatLng(l[0], l[1]);
+            },
+
+            __fixMarker: function() {
+                $('.leaflet-map-pane').css('z-index', '2 !important');
+                $('.leaflet-google-layer').css('z-index', '1 !important');
             }
         }
 
-        function load() {
-            var point = new google.maps.LatLng(1, 1);
-
-            var options = {
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            
-            location_map = new google.maps.Map(map[0], options);
-
-            var initial_position;
-
-            if (location_coordinate.val())
-            {
-                var l = location_coordinate.val().split(/,/);
-
-                if (l.length > 1)
-                {
-                    initial_position = new google.maps.LatLng(l[0], l[1]);
-                }
-            }
-
-            var marker = new google.maps.Marker({
-                map: location_map,
-                position: initial_position,
-                draggable: true
-            });
-
-            google.maps.event.addListener(marker, 'dragend', function(mouseEvent) {
-                savePosition(mouseEvent.latLng);
-            });
-
-            google.maps.event.addListener(location_map, 'click', function(mouseEvent){
-                marker.setPosition(mouseEvent.latLng);
-                savePosition(mouseEvent.latLng);
-            });
-
-            var no_change = false;
-
-            location_based.each(function(i, f)
-            {
-                var f = $(this),
-                    cb = function()
-                    {
-                        no_change = true;
-
-                        var lstr = [];
-
-                        location_based.each(function(){
-                            var b = $(this);
-
-                            if (b.is('select'))
-                                lstr.push(b.find('option:selected').html());
-                            else
-                                lstr.push(b.val())
-                        });
-
-                        if (lstr.length > 0 && suffix != '') 
-                            lstr.push(suffix);
-
-                        geocode(lstr.join(','), function(l){
-                            location_coordinate.val(l.lat()+','+l.lng());
-                            setTimeout(function(){ no_change = false; }, 2000);
-                        });
-                    };
-
-                if (f.is('select'))
-                    f.change(cb);
-                else
-                    f.keyup(cb);
-            });
-
-            // Prevents querying Google Maps everytime field changes
-            var location_coordinate_delay;
-
-            location_coordinate.keyup(function(){
-                if (no_change) return;
-                var latlng = $(this).val().split(/,/);
-                if (latlng.length < 2) return;
-                clearTimeout(location_coordinate_delay);
-                location_coordinate_delay = setTimeout(function(){
-                    var ll = new google.maps.LatLng(latlng[0], latlng[1]);
-                    location_map.panTo(ll);
-                    marker.setPosition(ll);
-                }, 1200);
-            });
-
-            function placeMarker(location) {
-                location_map.setZoom(zoom);
-                marker.setPosition(location);
-                location_map.setCenter(location);
-                savePosition(location);
-            }
-
-            function geocode(address, cb) {
-                var result;
-                var geocoder = new google.maps.Geocoder();
-                if (geocoder) {
-                    geocoder.geocode({"address": address}, function(results, status) {
-                        if (status == google.maps.GeocoderStatus.OK) {
-                            cb(results[0].geometry.location);
-                            placeMarker(results[0].geometry.location);
-                        }
-                    });
-                }
-            }
-
-            function geocode_reverse(location, cb) {
-                var geocoder = new google.maps.Geocoder();
-                if (geocoder) {
-                    geocoder.geocode({"latLng": location}, function(results, status) {
-                        if (status == google.maps.GeocoderStatus.OK) {
-                            cb(results[0].geometry.location);
-                            placeMarker(results[0].geometry.location);
-                        }
-                    });
-                }
-            }
-
-            placeMarker(initial_position);
+        return {
+            render: LocationField.render.bind(LocationField)
         }
-
-        load();
     }
 
-
     $('input[data-location-widget]').livequery(function(){
-        var $el = $(this), name = $el.attr('name'), pfx;
+        var $el = $(this), name = $el.attr('name'),
+            latLng = $el.parent().find(':text').val() || '0,0',
+            pfx;
 
         try {
             pfx = name.match(/-(\d+)-/)[1];
         } catch (e) {};
 
         var values = {
-            map: $el.attr('data-map'),
-            zoom: $el.attr('data-zoom'),
-            suffix: $el.attr('data-suffix'),
-            based_fields: $el.attr('data-based-fields')
+            mapId: $el.attr('data-map').replace(/^#/, ''),
+            basedFieldsSelector: $el.attr('data-based-fields')
         }
 
         if ( ! /__prefix__/.test(name)) {
@@ -166,11 +86,13 @@
             }
         }
 
-        var $map = $(values.map),
-            $based_fields = $(values.based_fields),
-            zoom = parseInt(values.zoom),
-            suffix = values.suffix;
+        values.suffix = $el.attr('data-suffix');
+        values.mapLatLng = latLng;
+        values.mapOptions = {
+            zoom: $el.attr('data-zoom')
+        };
 
-        location_field_load($map, $based_fields, zoom, suffix);
+        $.locationField(values).render();
+
     });
-});
+}($ || django.jQuery);
