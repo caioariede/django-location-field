5f2e0fd41d99dc647e797e851e97061eb8c8138a
Caio Ariede <caio.ariede@gmail.com>
2019-03-27 09:25:03 -0300

    Fix example project

--- example/dlf_app/models.py
+++ example/dlf_app/models.py
@@ -3,6 +3,12 @@ from location_field.models.plain import PlainLocationField
 
 
 class Place(models.Model):
-    parent_place = models.ForeignKey('self', null=True, blank=True)
+    parent_place = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+
     city = models.CharField(max_length=255)
     location = PlainLocationField(based_fields=['city'], zoom=7)
