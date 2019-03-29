dcb7c723a25dc2928a5a17ff481071a212fb8cc8
Alan Trick <me@alantrick.ca>
2019-03-28 20:45:44 -0700

    Fix changed checking for LocationFields
    See https://github.com/caioariede/django-location-field/issues/119

--- location_field/forms/spatial.py
+++ location_field/forms/spatial.py
@@ -10,3 +10,16 @@ class LocationField(PlainLocationField):
             return Point(float(lng), float(lat))
         except ValueError:
             return None
+
+    def _coerce(self, value):
+        """
+        Coerce coordinates for checking changed values.
+
+        Sometimes we have a Point, and sometimes we have a string.
+        In this case, turning the Points into strings is fine, because
+        we're only bound to have one SRID.
+        """
+        if isinstance(value, Point):
+            return '{y},{x}'.format(x=value.x, y=value.y)
+        return value
+    
